d41ab72195d7d1112fab8efad89c4eccbdbe23d0
acj <acj@d95f148e-e248-0410-ae72-c04dada67d05>
2008-07-07 13:06:55 +0000

    - comment and output cleanup for FindCycles
    git-svn-id: https://semistable.com/svn/debgraph@64 d95f148e-e248-0410-ae72-c04dada67d05

--- trunk/cyclictest.cc
+++ trunk/cyclictest.cc
@@ -8,7 +8,7 @@
 
 bool CyclicTest::run(Graph &g) {
 	ofstream htmlout("out/cyclictest.html");
-	htmlout << "<h1>CYCLICTEST</h1>\n\t";
+	htmlout << "<h1>CYCLICTEST</h1>\n";
 
 	findCyclesInDist(g, "stable", htmlout);
 	findCyclesInDist(g, "testing", htmlout);
@@ -25,21 +25,21 @@ void CyclicTest::findCyclesInDist(Graph &g, string distro, ofstream &out) {
 	cout << "Found " << cycles.size() << " cycles in Release:" << distro
 		 << endl;
 	for (size_t i = 0; i < cycles.size(); ++i) {
-		// Write out the original
+		// Write out the original Graph (includes HAS_VERSION et al.)
 		//sprintf(cycleName, "out/cyclictest-orig-%d.dot", i);
 		//ofstream dotfile_orig(cycleName);
 		//dotfile_orig << cycles[i].toGraphviz();
 		//dotfile_orig.close();
 		// Write out the reduced (BINARYNAME only) version
 		fc.mergePackageVersions(cycles[i]);
-
+		// Use the first node as a representative for this cycle
 		GraphIterator gIter = cycles[i].begin();
 		string pngFilename = "dot/" + distro + "-" + (*gIter)->getId() + ".png";
 		string dotFilename = "out/dot/" + distro + "-" + (*gIter)->getId() + ".dot";
 		// Graphviz output
-		ofstream dotfile(dotFilename.c_str());
-		dotfile << cycles[i].toGraphviz();
-		dotfile.close();
+		ofstream dotFile(dotFilename.c_str());
+		dotFile << cycles[i].toGraphviz();
+		dotFile.close();
 		// HTML output
 		out << "* <a href=\"" << pngFilename << "\">";
 		for (; gIter != cycles[i].end(); ++gIter) {
